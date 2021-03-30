7128805a578b4a908882e721eb7196b792acebca
hugobergqvist <hugo@bergqvist.com>
2021-03-30 17:51:40 +0200

    initial push

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,257 @@
 import React from 'react'
 
-import { ExampleComponent } from 'react-appear-on-scroll'
+import {
+  AppearingContainer,
+  AppearSequentialContainer
+} from 'react-appear-on-scroll'
 import 'react-appear-on-scroll/dist/index.css'
 
+const contentStyling = {
+  width: '250px',
+  height: '200px',
+  borderRadius: '4px',
+  backgroundColor: 'lightGrey',
+  flexDirection: 'column',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  boxShadow:
+    '0 1px 1px rgba(0,0,0,0.12), 0 2px 2px rgba(0,0,0,0.12), 0 4px 4px rgba(0,0,0,0.12)'
+}
+
+const spanStyling = {
+  display: 'flex',
+  justifyContent: 'space-evenly',
+  width: '100%',
+  marginTop: '10px',
+  flexWrap: 'wrap'
+}
+
+const textStyling = {
+  margin: '0px',
+  marginBottom: '4px'
+}
+
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontFamily: 'Verdana, sans-serif'
+      }}
+    >
+      <div
+        style={{
+          height: '100vh',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'flex-end',
+          alignItems: 'center'
+        }}
+      >
+        <p style={{ fontSize: '14pt', marginBottom: '10px' }}>
+          Scroll down to make the containers appear!
+        </p>
+        <div className='bounce'>
+          <div className='arrow down' />
+        </div>
+      </div>
+      <span style={spanStyling}>
+        <AppearingContainer
+          animationType='fromLeft'
+          transitionType='ease'
+          fading
+        >
+          <div style={{ ...contentStyling, backgroundColor: '#4cc9f0' }}>
+            <p style={textStyling}>animationType: fromLeft</p>
+            <p style={textStyling}>transitionType: ease</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='fromLeft'
+          transitionType='smooth'
+          stayVisible
+        >
+          <div style={{ ...contentStyling, backgroundColor: '#4cc9f0' }}>
+            <p style={textStyling}>animationType: fromLeft</p>
+            <p style={textStyling}>transitionType: smooth</p>
+            <p style={textStyling}>fading: false</p>
+            <p style={textStyling}>stayVisible: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='fromLeft'
+          transitionType='bouncy'
+          fading
+        >
+          <div style={{ ...contentStyling, backgroundColor: '#4cc9f0' }}>
+            <p style={textStyling}>animationType: fromLeft</p>
+            <p style={textStyling}>transitionType: bouncy</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+      </span>
+      <span style={spanStyling}>
+        <AppearingContainer
+          animationType='fromTop'
+          transitionType='smooth'
+          fading
+        >
+          <div style={{ ...contentStyling, backgroundColor: '#4895ef' }}>
+            <p style={textStyling}>animationType: fromTop</p>
+            <p style={textStyling}>transitionType: smooth</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='fromTop'
+          transitionType='ease'
+          fading
+        >
+          <div style={{ ...contentStyling, backgroundColor: '#4895ef' }}>
+            <p style={textStyling}>animationType: fromTop</p>
+            <p style={textStyling}>transitionType: ease</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='fromTop'
+          transitionType='bouncy'
+          fading
+        >
+          <div style={{ ...contentStyling, backgroundColor: '#4895ef' }}>
+            <p style={textStyling}>animationType: fromTop</p>
+            <p style={textStyling}>transitionType: bouncy</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+      </span>
+      <span style={spanStyling}>
+        <AppearingContainer
+          animationType='scale'
+          transitionType='smooth'
+          fading
+        >
+          <div
+            style={{
+              ...contentStyling,
+              backgroundColor: '#4361ee',
+              color: 'white'
+            }}
+          >
+            <p style={textStyling}>animationType: scale</p>
+            <p style={textStyling}>transitionType: smooth</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer animationType='scale' transitionType='ease' fading>
+          <div
+            style={{
+              ...contentStyling,
+              backgroundColor: '#4361ee',
+              color: 'white'
+            }}
+          >
+            <p style={textStyling}>animationType: scale</p>
+            <p style={textStyling}>transitionType: ease</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='scale'
+          transitionType='bouncy'
+          fading
+        >
+          <div
+            style={{
+              ...contentStyling,
+              backgroundColor: '#4361ee',
+              color: 'white'
+            }}
+          >
+            <p style={textStyling}>animationType: scale</p>
+            <p style={textStyling}>transitionType: bouncy</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+      </span>
+      <p>
+        This is a 'AppearSequentialContainer' which adds a longer and longer
+        transition delay to each item, making them appear in sequence
+      </p>
+      <AppearSequentialContainer
+        style={{
+          backgroundColor: '#480ca8',
+          padding: '10px',
+          borderRadius: '4px'
+        }}
+      >
+        <AppearingContainer
+          animationType='scale'
+          transitionType='bouncy'
+          fading
+        >
+          <div
+            style={{
+              ...contentStyling,
+              backgroundColor: '#b5179e',
+              color: 'white'
+            }}
+          >
+            <p style={textStyling}>animationType: scale</p>
+            <p style={textStyling}>transitionType: bouncy</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='scale'
+          transitionType='bouncy'
+          fading
+        >
+          <div
+            style={{
+              ...contentStyling,
+              backgroundColor: '#b5179e',
+              color: 'white'
+            }}
+          >
+            <p style={textStyling}>animationType: scale</p>
+            <p style={textStyling}>transitionType: bouncy</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+        <AppearingContainer
+          animationType='scale'
+          transitionType='bouncy'
+          fading
+        >
+          <div
+            style={{
+              ...contentStyling,
+              backgroundColor: '#b5179e',
+              color: 'white'
+            }}
+          >
+            <p style={textStyling}>animationType: scale</p>
+            <p style={textStyling}>transitionType: bouncy</p>
+            <p style={textStyling}>fading: true</p>
+          </div>
+        </AppearingContainer>
+      </AppearSequentialContainer>
+      <div
+        style={{
+          height: '10vh',
+          width: '100%',
+          marginTop: '10px'
+        }}
+      />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,168 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { useState, useEffect, useRef } from 'react'
+import PropTypes from 'prop-types'
+import './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const AppearingContainer = ({
+  children,
+  parentStyling,
+  wrapperStyling,
+  delay = 0,
+  transitionType = 'ease',
+  fading = false,
+  stayVisible = false,
+  animationType = 'fromLeft',
+  containerMargin = 15
+}) => {
+  const appearingDiv = useRef()
+
+  const [visible, setVisible] = useState(false)
+
+  let contentWidth = parseInt(children.props.style.width.replace('px', ''))
+  let contentHeight = parseInt(children.props.style.height.replace('px', ''))
+
+  let wrapperWidth = contentWidth + containerMargin
+  let wrapperHeight = contentHeight + containerMargin
+
+  useEffect(() => {
+    window.addEventListener('scroll', isVisible)
+
+    return () => window.removeEventListener('scroll', isVisible)
+  }, [])
+
+  const isVisible = () => {
+    const rect = appearingDiv.current.getBoundingClientRect()
+    if (
+      rect.top >= -rect.height / 2 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+      setVisible(true)
+    } else if (!stayVisible) {
+      setVisible(false)
+    }
+  }
+
+  return (
+    <div
+      ref={appearingDiv}
+      className={`appearingDiv  ${fading && 'fade'}`}
+      style={{
+        ...parentStyling,
+        overflow: animationType === 'scale' ? '' : 'hidden',
+        opacity: fading ? (visible ? 1 : 0) : 1,
+        width: parentStyling
+          ? parentStyling.width
+            ? parentStyling.width
+            : wrapperWidth + 'px'
+          : wrapperWidth + 'px',
+        height: parentStyling
+          ? parentStyling.height
+            ? parentStyling.height
+            : wrapperHeight + 'px'
+          : wrapperHeight + 'px'
+      }}
+    >
+      <div
+        className={`contentWrapper ${transitionType}`}
+        style={{
+          ...wrapperStyling,
+          transitionDelay: `${delay}ms`,
+
+          left:
+            animationType === 'fromLeft'
+              ? visible
+                ? '0px'
+                : `-${contentWidth + containerMargin}px`
+              : 'auto',
+          right:
+            animationType === 'fromRight'
+              ? visible
+                ? '0px'
+                : `-${contentWidth + containerMargin}px`
+              : 'auto',
+          top:
+            animationType === 'fromTop'
+              ? visible
+                ? '0px'
+                : `-${contentHeight + containerMargin}px`
+              : 'auto',
+          bottom:
+            animationType === 'fromBottom'
+              ? visible
+                ? '0px'
+                : `-${contentHeight + containerMargin}px`
+              : 'auto',
+          transform:
+            animationType === 'scale'
+              ? visible
+                ? 'scale(1)'
+                : 'scale(0)'
+              : 'auto'
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export const AppearSequentialContainer = ({ children, style }) => {
+  const [content, setContent] = useState(null)
+
+  useEffect(() => {
+    let tempContent = []
+    let delay = 0
+
+    if (Array.isArray(children)) {
+      children.forEach((child, index) => {
+        tempContent.push(
+          React.cloneElement(child, {
+            delay: delay,
+            key: `appearingContainer-${index}`
+          })
+        )
+        delay += 100
+      })
+    } else {
+      tempContent.push(
+        React.cloneElement(children, {
+          delay: delay,
+          key: `appearingContainer`
+        })
+      )
+    }
+
+    setContent(tempContent)
+  }, [children])
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        ...style
+      }}
+    >
+      {content}
+    </div>
+  )
+}
+
+AppearingContainer.propTypes = {
+  children: PropTypes.object.isRequired,
+  parentStyling: PropTypes.object,
+  wrapperStyling: PropTypes.object,
+  delay: PropTypes.number,
+  transitionType: PropTypes.oneOf(['ease', 'bouncy', 'smooth']),
+  fading: PropTypes.bool,
+  stayVisible: PropTypes.bool,
+  animationType: PropTypes.oneOf([
+    'fromLeft',
+    'fromRight',
+    'fromTop',
+    'fromBottom',
+    'scale'
+  ]),
+  containerMargin: PropTypes.number
 }
