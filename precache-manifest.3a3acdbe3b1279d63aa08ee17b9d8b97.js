e6e80e0b7cfc64e7aeaacbbde7691fb5cc61c5fb
hugobergqvist <hugo@bergqvist.com>
2021-03-30 22:40:08 +0200

    Updates

---
+++ precache-manifest.3a3acdbe3b1279d63aa08ee17b9d8b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5f2be5c04e9c28c32efaf3637d1045",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4323e9052e370ab1b688",
+    "url": "./static/css/main.2afa6851.chunk.css"
+  },
+  {
+    "revision": "9a28938db5b8d8e22e7a",
+    "url": "./static/js/2.45f573c7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.45f573c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4323e9052e370ab1b688",
+    "url": "./static/js/main.5d4dd8eb.chunk.js"
+  },
+  {
+    "revision": "c9feebfb35440ff6f119",
+    "url": "./static/js/runtime-main.b10f6ec8.js"
+  }
+]);
